387e018b68a711783832cbb2f17d14708c36ea7c
yzibin <yzibin@google.com>
2020-10-13 13:30:20 +0300

    eslint and check all files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,47 @@
+// eslint-disable-next-line
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
+    // https://github.com/expo/expo/tree/master/packages/eslint-config-universe
     // Choose from universe/native, universe/node, universe/web
-    'universe/native',
-    'universe/web',
+    // 'universe/native',
+    // 'universe/web',
+    'eslint:recommended',
     'plugin:react/recommended',
     'google',
+    // Must be last so it gets the chance to override other configs.
+    // see https://github.com/prettier/eslint-config-prettier
+    'prettier',
   ],
+  // See https://github.com/yannickcr/eslint-plugin-react#configuration
+  settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: 'React', // Pragma to use, default to "React"
+      fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+      flowVersion: '0.53', // Flow version
+    },
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`.
+      // If this isn't set, any propTypes wrapped in a function will be skipped.
+      'forbidExtraProps',
+      {property: 'freeze', object: 'Object'},
+      {property: 'myFavoriteWrapper'},
+    ],
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      {name: 'Link', linkAttribute: 'to'},
+    ],
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
