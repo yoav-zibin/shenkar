573086b6a3994719e744ddb5fdb6c351ec32bb25
Yoav Zibin <yoav.zibin@gmail.com>
2020-11-15 15:47:04 +0200

    feat: add reactnativeelements and yoav_zibin_component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {MyComponent} from './hw2_react_components/yoav_zibin_component';
+import {ThemeProvider} from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <MyComponent title="some title" />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
