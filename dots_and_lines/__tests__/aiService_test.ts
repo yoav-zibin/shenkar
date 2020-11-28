3993f886481280548fd3776086ce86b574fc71cb
Avishai Nudelman <avishainu@gmail.com>
2020-11-28 20:10:25 +0200

    2811

--- dots_and_lines/__tests__/aiService_test.ts
+++ dots_and_lines/__tests__/aiService_test.ts
@@ -8,31 +8,31 @@ describe('aiService', function () {
     console.log('case1');
     const state = gameLogic.getInitialState();
     const boardBeforeLastMove = deepClone(state.board);
-    boardBeforeLastMove.paintedLinesAmount = 23;
+    boardBeforeLastMove.paintedLinesAmount = 21;
     boardBeforeLastMove.score = {
-      PLAYER_1: 2,
-      PLAYER_2: 6,
+      PLAYER_1: 4,
+      PLAYER_2: 0,
     };
     boardBeforeLastMove.horizontalLines = [
       [true, true, true],
       [true, true, true],
-      [true, true, true],
+      [false, true, true],
       [true, true, true],
     ];
     boardBeforeLastMove.verticalLines = [
-      [true, true, true, true],
+      [true, true, false, true],
       [true, true, true, true],
       [true, true, true, false],
     ];
     boardBeforeLastMove.color = [
-      [cellColor.PLAYER_1, cellColor.PLAYER_1, cellColor.PLAYER_1],
-      [cellColor.PLAYER_1, cellColor.PLAYER_1, cellColor.PLAYER_1],
-      [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.NONE],
+      [cellColor.PLAYER_1, cellColor.NONE, cellColor.NONE],
+      [cellColor.NONE, cellColor.PLAYER_1, cellColor.PLAYER_1],
+      [cellColor.NONE, cellColor.PLAYER_1, cellColor.NONE],
     ];
     boardBeforeLastMove.cellPaintedLines = [
-      [4, 4, 4],
-      [4, 4, 4],
-      [4, 4, 3],
+      [4, 3, 3],
+      [3, 4, 4],
+      [3, 4, 3],
     ];
 
     const delta = {
@@ -41,17 +41,6 @@ describe('aiService', function () {
       col: 3,
     };
     const possibleMoves = aiService.getPossibleMoves({board: boardBeforeLastMove, delta});
-    const boardAfterLastMove = gameLogic.updateBoard(boardBeforeLastMove, gameLogic.lineDirection.VERTICAL, 2, 3);
-    const expectedMove: IMove<gameLogic.IState> = {
-      endMatchScores: [3, 6],
-      state: {board: boardAfterLastMove, delta},
-      turnIndex: 0,
-    };
-    // { set: { key: 'board', value: boardAfterLastMove } },
-    // { set: { key: 'delta', value: { dir: 'hor', row: 3, col: 2 } } }];
-    console.log(possibleMoves[0].state.board);
-    console.log(expectedMove.state.board);
-    expect(deepEquals(possibleMoves, [expectedMove])).toBe(true);
-    // expect(possibleMoves.length).toBe(1);
+    expect(possibleMoves.length).toBe(3);
   });
 });
