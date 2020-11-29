546221b8182d9090cbcc8af977eba2e246ee7d26
barakdaniel <barakdaniel22@gmail.com>
2020-11-29 14:54:14 +0200

    Updating Go Game

---
+++ go_game/aiService.ts
@@ -0,0 +1,29 @@
+import {AiService, IMove} from '../common/common';
+import {IState, createMove, getInitialState} from './gameLogic';
+
+export function getPossibleMoves(state: IState, turnIndex: number): IMove<IState>[] {
+  const possibleMoves: IMove<IState>[] = [];
+  for (let i = 0; i < state.board.length; i++) {
+    for (let j = 0; j < state.board.length; j++) {
+      try {
+        const delta = {row: i, col: j};
+        // STILL WRONG! :
+        if (state.board[i][j] == '') possibleMoves.push(createMove(state.board, 0, null, delta, turnIndex, delta));
+      } catch (e) {
+        // The cell in that position was full.
+      }
+    }
+  }
+  return possibleMoves;
+}
+
+export function getStateScoreForIndex0(): number {
+  /* state: IState, turnIndex: number */
+  return 0;
+}
+
+export const aiService: AiService<IState> = {
+  initialState: getInitialState(),
+  getPossibleMoves,
+  getStateScoreForIndex0,
+};
