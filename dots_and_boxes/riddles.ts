3bece89d9971f60478dd9f61372b03d12e4ebdb7
Avishai Nudelman <avishainu@gmail.com>
2020-12-08 22:54:23 +0200

    trying to commit

--- dots_and_boxes/riddles.ts
+++ dots_and_boxes/riddles.ts
@@ -48,18 +48,121 @@ export const riddleLevels: RiddlesLevel<IState>[] = [
       }),
     ],
   },
-  //   {
-  //     levelLocalizeId: 'DOTS_AND_BOXES_LEVEL2',
-  //     difficulty: 'EASY',
-  //     maxMovesNum: 2,
-  //     turnIndex: 1,
-  //     riddles: [],
-  //   },
-  //   {
-  //     levelLocalizeId: 'DOTS_AND_BOXES_LEVEL3',
-  //     maxMovesNum: 3,
-  //     difficulty: 'MEDIUM',
-  //     turnIndex: 0,
-  //     riddles: [],
-  //   },
+  {
+    levelLocalizeId: 'DOTS_AND_BOXES_LEVEL2',
+    difficulty: 'EASY',
+    maxMovesNum: 1,
+    turnIndex: 0,
+    riddles: [
+      r('h11', {
+        paintedLinesAmount: 23,
+        score: {
+          PLAYER_1: 4,
+          PLAYER_2: 0,
+        },
+        horizontalLines: [
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+        ],
+        verticalLines: [
+          [true, true, true, true],
+          [true, true, true, true],
+          [true, true, true, false],
+        ],
+        color: [
+          [cellColor.PLAYER_1, cellColor.PLAYER_2, cellColor.PLAYER_2],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.PLAYER_1],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.NONE],
+        ],
+        cellPaintedLines: [
+          [4, 4, 4],
+          [4, 4, 4],
+          [4, 4, 3],
+        ],
+        isGameOver: false,
+        size: 3,
+        turn: player.ONE,
+      }),
+    ],
+  },
+  {
+    levelLocalizeId: 'DOTS_AND_BOXES_LEVEL3',
+    maxMovesNum: 1,
+    difficulty: 'MEDIUM',
+    turnIndex: 0,
+    riddles: [
+      r('h11', {
+        paintedLinesAmount: 23,
+        score: {
+          PLAYER_1: 4,
+          PLAYER_2: 0,
+        },
+        horizontalLines: [
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+        ],
+        verticalLines: [
+          [true, true, true, true],
+          [true, true, true, true],
+          [true, true, true, false],
+        ],
+        color: [
+          [cellColor.PLAYER_1, cellColor.PLAYER_2, cellColor.PLAYER_2],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.PLAYER_1],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.NONE],
+        ],
+        cellPaintedLines: [
+          [4, 4, 4],
+          [4, 4, 4],
+          [4, 4, 3],
+        ],
+        isGameOver: false,
+        size: 3,
+        turn: player.ONE,
+      }),
+    ],
+  },
+  {
+    levelLocalizeId: 'DOTS_AND_BOXES_LEVEL4',
+    maxMovesNum: 1,
+    difficulty: 'MEDIUM',
+    turnIndex: 0,
+    riddles: [
+      r('h11', {
+        paintedLinesAmount: 23,
+        score: {
+          PLAYER_1: 4,
+          PLAYER_2: 0,
+        },
+        horizontalLines: [
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+          [true, true, true],
+        ],
+        verticalLines: [
+          [true, true, true, true],
+          [true, true, true, true],
+          [true, true, true, false],
+        ],
+        color: [
+          [cellColor.PLAYER_1, cellColor.PLAYER_2, cellColor.PLAYER_2],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.PLAYER_1],
+          [cellColor.PLAYER_2, cellColor.PLAYER_1, cellColor.NONE],
+        ],
+        cellPaintedLines: [
+          [4, 4, 4],
+          [4, 4, 4],
+          [4, 4, 3],
+        ],
+        isGameOver: false,
+        size: 3,
+        turn: player.ONE,
+      }),
+    ],
+  },
 ];
