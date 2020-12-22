168da178dc9b935d41eddf063f89ffd2baf2140a
TomerMizrachi <48449324+TomerMizrachi@users.noreply.github.com>
2020-12-22 14:00:27 +0200

    try

---
+++ checkers/components/Game.tsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import {Animated, StyleSheet, TouchableWithoutFeedback, View, Image, ImageBackground, ViewStyle} from 'react-native';
+
+import {GameModule, GameProps} from '../../common/common';
+
+import {createMove, IState, getInitialState, checkRiddleData} from '../gameLogic';
+import {getPossibleMoves, getStateScoreForIndex0} from '../aiService';
+import {riddleLevels} from '../riddles';
+
+const hintLineColor = 'green';
+
+const styles = StyleSheet.create({
+    
+  // See how to have a square component using aspectRatio=1
+  // https://reactnative.fun/2017/06/21/ratio/
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  container: {
+    margin: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boardImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'stretch',
+  },
+  boardRowsContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+    padding: 0,
+    margin: 0,
+  },
+  boardCellsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+    padding: 0,
+    margin: 0,
+  },
+  boardCell: {
+    width: '12.5%',
+    height: '100%',
+    padding: 0,
+    margin: 0,
+  },
+  pieceImage: {
+    marginTop: '14%',
+    marginLeft: '12%',
+    width: '80%',
+    height: '80%',
+  },
+//   hintLineCol: {
+//     position: 'absolute',
+//     width: 8,
+//     height: '100%',
+//     backgroundColor: hintLineColor,
+//   },
+//   hintLineRow: {
+//     position: 'absolute',
+//     width: '100%',
+//     height: 8,
+//     backgroundColor: hintLineColor,
+//   },
+//   hintLineDiagonal1: {
+//     position: 'absolute',
+//     width: 8,
+//     height: '100%',
+//     backgroundColor: hintLineColor,
+//     transform: [{rotate: '135deg'}],
+//     left: '50%',
+//   },
+//   hintLineDiagonal2: {
+//     position: 'absolute',
+//     width: 8,
+//     height: '100%',
+//     backgroundColor: hintLineColor,
+//     transform: [{rotate: '45deg'}],
+//     left: '50%',
+//   },
+});
+
+export default function getCheckersGameModule(): GameModule<IState> {
+    return {
+      gameId: 'checkers',
+      gameLocalizeId: 'CHECKERS_GAME_NAME',
+      initialState: getInitialState(),
+      component: CheckersComponent,
+      riddleLevels,
+      getPossibleMoves,
+      getStateScoreForIndex0,
+      checkRiddleData,
+    };
+  }
+
+const CheckersComponent: React.FunctionComponent<GameProps<IState>> = (props: GameProps<IState>) => {
+  const {move, setMove, yourPlayerIndex, showHint} = props;
+  const {turnIndex, state} = move;
+  const {riddleData, board, miniMoves} = state;
+  // console.log('Render Checkers miniMoves=', miniMoves);
+
+  function clickedOn(row: number, col: number) {
+    if (turnIndex != yourPlayerIndex) {
+      return;
+    }
+    try {
+      const move = createMove(board, miniMoves, turnIndex);
+      setMove(move);
+    } catch (e) {
+      console.info('Cell is already full in position:', row, col);
+    }
+  }
+
+  const rows = [0, 1, 2, 3, 4, 5, 6, 7];
+  const cols = [0, 1, 2, 3, 4, 5, 6, 7];
+
+  let hintLine = null;
+
+
+  function getPiece(r: number, c: number) {
+    if (board[r][c] == '--') return null;
+    if (board[r][c] == 'DS') return null;
+    return (
+      <Image
+        style = {styles.pieceImage}
+        source = {board[r][c] == 'WM' ? require('../imgs/white_man.png'):require('../imgs/black_man.png') }
+      />
+    );
+  }
+    return (
+        <View style={styles.container}>
+          <View style={styles.fixedRatio}>
+            <ImageBackground style={styles.boardImage} source={require('../imgs/board.jpg')}>
+              <View style={styles.boardRowsContainer}>
+                {rows.map((r) => (
+                  <View key={r} style={styles.boardCellsContainer}>
+                    {cols.map((c) => (
+                      <TouchableWithoutFeedback key={c} onPress={() => clickedOn(r, c)}>
+                        <View style={styles.boardCell}>
+                          {getPiece(r, c)}
+                          </View>
+                      </TouchableWithoutFeedback>
+                    ))}
+                  </View>
+                ))}
+              </View>
+            </ImageBackground>
+            {hintLine}
+          </View>
+        </View>
+      );
+}
---
+++ checkers/riddles.ts
@@ -0,0 +1,68 @@
+import {RiddlesLevel} from '../common/common';
+import {Board, IState, MiniMove, RiddleData} from './gameLogic';
+
+function r(riddleData: RiddleData, board: Board, miniMoves: MiniMove[]): IState {
+  return {
+    board: board,
+    miniMoves: miniMoves,
+    riddleData: riddleData,
+  };
+}
+
+export const riddleLevels: RiddlesLevel<IState>[] = [
+    {
+      levelLocalizeId: 'CHECKERS_LEVEL1',
+      difficulty: 'EASY',
+      maxMovesNum: 1,
+      turnIndex: 0,
+      riddles: [
+        r('r1', [
+          ['--', 'BM', '--', 'BM', '--', 'BM', '--', 'BM'],
+          ['BM', '--', 'BM', '--', 'BM', '--', 'BM', '--'],
+          ['--', 'BM', '--', 'DS', '--', 'BM', '--', 'BM'],
+          ['DS', '--', 'BM', '--', 'DS', '--', 'DS', '--'],
+          ['--', 'WM', '--', 'DS', '--', 'DS', '--', 'DS'],
+          ['DS', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+          ['--', 'WM', '--', 'WM', '--', 'WM', '--', 'WM'],
+          ['WM', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+        ],[/*minimoves*/]),
+      ],
+    },
+    {
+      levelLocalizeId: 'CHECKERS_LEVEL2',
+      difficulty: 'MEDIUM',
+      maxMovesNum: 1,
+      turnIndex: 1,
+      riddles: [
+        r('r1', [
+          ['--', 'BM', '--', 'BM', '--', 'BM', '--', 'BM'],
+          ['BM', '--', 'BM', '--', 'BM', '--', 'BM', '--'],
+          ['--', 'BM', '--', 'BM', '--', 'BM', '--', 'BM'],
+          ['DS', '--', 'DS', '--', 'DS', '--', 'DS', '--'],
+          ['--', 'DS', '--', 'DS', '--', 'DS', '--', 'DS'],
+          ['WM', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+          ['--', 'WM', '--', 'WM', '--', 'WM', '--', 'WM'],
+          ['WM', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+        ],[/*minimoves*/]),
+      ],
+    },
+    {
+      levelLocalizeId: 'CHECKERS_LEVEL3',
+      maxMovesNum: 3,
+      difficulty: 'HARD',
+      turnIndex: 0,
+      riddles: [
+        r('r1', [
+          ['--', 'BM', '--', 'BM', '--', 'BM', '--', 'BM'],
+          ['BM', '--', 'BM', '--', 'BM', '--', 'BM', '--'],
+          ['--', 'BM', '--', 'BM', '--', 'BM', '--', 'BM'],
+          ['DS', '--', 'DS', '--', 'DS', '--', 'DS', '--'],
+          ['--', 'DS', '--', 'DS', '--', 'DS', '--', 'DS'],
+          ['WM', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+          ['--', 'WM', '--', 'WM', '--', 'WM', '--', 'WM'],
+          ['WM', '--', 'WM', '--', 'WM', '--', 'WM', '--'],
+        ],[/*minimoves*/]),
+      ],
+    },
+  ];
+  
