d6c993399309f1034fb731d476ae34cd5e55275c
chen gutman <chengutman0@gmail.com>
2021-01-14 18:18:33 +0200

    Choose game screen commit
    Choose game screen commit

--- common/ChooseGameScreen.tsx
+++ common/ChooseGameScreen.tsx
@@ -1,29 +1,64 @@
 import React from 'react';
-import {FlatListChooser} from './FlatListChooser';
 import {getAllGameModules} from './gameModules';
 import {navigateNextFrame, useStoreContext} from './store';
 import {TitleBar} from './TitleBar';
 import {useNavigation} from '@react-navigation/native';
-import {commonStyles} from './common';
-import {View} from 'react-native';
+import {localize, LocalizeId} from './localize';
+import {View, FlatList, StyleSheet, Text, Image} from 'react-native';
+import {PageTitle} from './PageTitle';
+import {IMAGES} from './imgs/imagesRequires';
 
 export function ChooseGameScreen() {
-  const {dispatch} = useStoreContext();
+  const {appState, dispatch} = useStoreContext();
   const navigation = useNavigation();
   const choices = getAllGameModules().map((i) => {
     return {id: i.gameId, localizeId: i.gameLocalizeId};
   });
-  console.log('Render ChooseGameScreen.');
   return (
-    <View style={commonStyles.screen}>
+    <View>
       <TitleBar />
-      <FlatListChooser
-        choices={choices}
-        setChoice={(choice) => {
-          dispatch({setSelectedGameId: choice.id});
-          navigateNextFrame('ChooseActivity', navigation);
-        }}
-      />
+      <PageTitle />
+      <View style={ChooseGamePageStyle.flatList}>
+        <FlatList
+          data={choices}
+          numColumns={2}
+          keyExtractor={(choice) => choice.id}
+          renderItem={({item}) => (
+            <View style={ChooseGamePageStyle.flatListItemContainer}>
+              <Image style={ChooseGamePageStyle.tinyLogo} source={IMAGES[`${item.id}`]} />
+              <Text
+                key={item.id}
+                style={ChooseGamePageStyle.flatListItemText}
+                onPress={() => {
+                  dispatch({setSelectedGameId: item.id});
+                  navigateNextFrame('ChooseActivity', navigation);
+                }}>
+                {localize(item.localizeId, appState)}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
+const ChooseGamePageStyle = StyleSheet.create({
+  flatList: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  flatListItemContainer: {
+    marginBottom: '5%',
+    marginTop: '5%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  flatListItemText: {
+    marginTop: '2%',
+    fontSize: 18,
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+});
