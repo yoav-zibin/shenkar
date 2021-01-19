2d76d590b33ebeff63f03bdfec4394a533e5462b
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2021-01-19 18:51:56 +0200

    Update ChooseActivityScreen.tsx

--- common/ChooseActivityScreen.tsx
+++ common/ChooseActivityScreen.tsx
@@ -1,20 +1,14 @@
-import { Choice } from "./FlatListChooser";
-import React from "react";
-import { Activity, navigateNextFrame, useStoreContext } from "./store";
-import { localize, LocalizeId } from "./localize";
-import { TitleBar } from "./TitleBar";
-import { findGameModule } from "./gameModules";
-import { useNavigation } from "@react-navigation/native";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  Text,
-  ImageBackground,
-} from "react-native";
+import {Choice} from './FlatListChooser';
+import React from 'react';
+import {Activity, navigateNextFrame, useStoreContext} from './store';
+import {localize, LocalizeId} from './localize';
+import {TitleBar} from './TitleBar';
+import {findGameModule} from './gameModules';
+import {useNavigation} from '@react-navigation/native';
+import {View, StyleSheet, FlatList, Text, ImageBackground} from 'react-native';
 
 export function ChooseActivityScreen() {
-  const { appState, dispatch } = useStoreContext();
+  const {appState, dispatch} = useStoreContext();
   const navigation = useNavigation();
   if (!appState.selectedGameId) return null;
   const gameModule = findGameModule(appState.selectedGameId);
@@ -25,15 +19,12 @@ export function ChooseActivityScreen() {
     const level = riddleLevels[levelIndex];
     choices.push(
       getActivityChoice(level.levelLocalizeId, {
-        riddleActivity: { levelIndex, riddleIndex: 0 },
+        riddleActivity: {levelIndex, riddleIndex: 0},
       })
     );
   }
 
-  function getActivityChoice(
-    localizeId: LocalizeId,
-    activity: Activity
-  ): Choice<Activity> {
+  function getActivityChoice(localizeId: LocalizeId, activity: Activity): Choice<Activity> {
     return {
       id: localizeId,
       text: localize(localizeId, appState),
@@ -41,36 +32,26 @@ export function ChooseActivityScreen() {
     };
   }
 
-  choices.push(
-    getActivityChoice("AGAINST_COMPUTER", { activityType: "AGAINST_COMPUTER" })
-  );
-  choices.push(
-    getActivityChoice("PASS_AND_PLAY", { activityType: "PASS_AND_PLAY" })
-  );
+  choices.push(getActivityChoice('AGAINST_COMPUTER', {activityType: 'AGAINST_COMPUTER'}));
+  choices.push(getActivityChoice('PASS_AND_PLAY', {activityType: 'PASS_AND_PLAY'}));
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.logoBack}
-        source={require("./activityBackImage.png")}
-      >
+      <ImageBackground style={styles.logoBack} source={require('./activityBackImage.png')}>
         <TitleBar />
         <FlatList
           contentContainerStyle={styles.list}
           data={choices}
           keyExtractor={(choice) => choice.id}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <Text
               key={item.id}
               style={styles.listItem}
               onPress={() => {
-                dispatch({ setActivity: item.data as Activity });
-                navigateNextFrame("PlayArea", navigation);
-              }}
-            >
-              {"localizeId" in item
-                ? localize(item.localizeId, appState)
-                : item.text}
+                dispatch({setActivity: item.data as Activity});
+                navigateNextFrame('PlayArea', navigation);
+              }}>
+              {'localizeId' in item ? localize(item.localizeId, appState) : item.text}
             </Text>
           )}
         />
@@ -81,26 +62,26 @@ export function ChooseActivityScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F6EEDF",
+    backgroundColor: '#F6EEDF',
     flex: 1,
   },
   list: {
     //  backgroundColor: 'red',
     flex: 1,
-    justifyContent: "space-evenly",
-    alignContent: "center",
+    justifyContent: 'space-evenly',
+    alignContent: 'center',
   },
   listItem: {
-    backgroundColor: "#e3c794",
-    color: "#F6742B",
-    textAlign: "center",
+    backgroundColor: '#e3c794',
+    color: '#F6742B',
+    textAlign: 'center',
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     height: 80,
     width: 400,
-    alignSelf: "center",
+    alignSelf: 'center',
     borderRadius: 20,
-    shadowColor: " rgba(235,188,112,0.82)",
+    shadowColor: ' rgba(235,188,112,0.82)',
     shadowOffset: {
       width: 0,
       height: 5,
@@ -112,7 +93,7 @@ const styles = StyleSheet.create({
   },
   logoBack: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
 });
