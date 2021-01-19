97c1df30bba2969c89d4b62a3d45d555275d1659
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2021-01-19 17:19:40 +0200

    update Activity Screen UI

--- common/ChooseActivityScreen.tsx
+++ common/ChooseActivityScreen.tsx
@@ -1,14 +1,20 @@
-import {Choice, FlatListChooser} from './FlatListChooser';
-import React from 'react';
-import {Activity, navigateNextFrame, useStoreContext} from './store';
-import {localize, LocalizeId} from './localize';
-import {TitleBar} from './TitleBar';
-import {findGameModule} from './gameModules';
-import {useNavigation} from '@react-navigation/native';
-import {View, StyleSheet, FlatList, Text, ImageBackground} from 'react-native';
+import { Choice } from "./FlatListChooser";
+import React from "react";
+import { Activity, navigateNextFrame, useStoreContext } from "./store";
+import { localize, LocalizeId } from "./localize";
+import { TitleBar } from "./TitleBar";
+import { findGameModule } from "./gameModules";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  ImageBackground,
+} from "react-native";
 
 export function ChooseActivityScreen() {
-  const {appState, dispatch} = useStoreContext();
+  const { appState, dispatch } = useStoreContext();
   const navigation = useNavigation();
   if (!appState.selectedGameId) return null;
   const gameModule = findGameModule(appState.selectedGameId);
@@ -19,12 +25,15 @@ export function ChooseActivityScreen() {
     const level = riddleLevels[levelIndex];
     choices.push(
       getActivityChoice(level.levelLocalizeId, {
-        riddleActivity: {levelIndex, riddleIndex: 0},
+        riddleActivity: { levelIndex, riddleIndex: 0 },
       })
     );
   }
 
-  function getActivityChoice(localizeId: LocalizeId, activity: Activity): Choice<Activity> {
+  function getActivityChoice(
+    localizeId: LocalizeId,
+    activity: Activity
+  ): Choice<Activity> {
     return {
       id: localizeId,
       text: localize(localizeId, appState),
@@ -32,75 +41,78 @@ export function ChooseActivityScreen() {
     };
   }
 
-  choices.push(getActivityChoice('AGAINST_COMPUTER', {activityType: 'AGAINST_COMPUTER'}));
-  choices.push(getActivityChoice('PASS_AND_PLAY', {activityType: 'PASS_AND_PLAY'}));
+  choices.push(
+    getActivityChoice("AGAINST_COMPUTER", { activityType: "AGAINST_COMPUTER" })
+  );
+  choices.push(
+    getActivityChoice("PASS_AND_PLAY", { activityType: "PASS_AND_PLAY" })
+  );
 
   return (
     <View style={styles.container}>
       <ImageBackground
         style={styles.logoBack}
-        source={require(
-          './activityBackImage.png'
-        )}
+        source={require("./activityBackImage.png")}
       >
-      <TitleBar />
-
-  <FlatList
-      contentContainerStyle={styles.list}
-      data={choices}
-      keyExtractor={(choice) => choice.id}
-      renderItem={({item}) => (
-        <Text key={item.id} style={styles.listItem} onPress={() => {
-          dispatch({setActivity: item.data as Activity});
-          navigateNextFrame('PlayArea', navigation);
-        }}>
-          {'localizeId' in item ? localize(item.localizeId, appState) : item.text}
-        </Text>
-      )}
-    />
-</ImageBackground>
+        <TitleBar />
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={choices}
+          keyExtractor={(choice) => choice.id}
+          renderItem={({ item }) => (
+            <Text
+              key={item.id}
+              style={styles.listItem}
+              onPress={() => {
+                dispatch({ setActivity: item.data as Activity });
+                navigateNextFrame("PlayArea", navigation);
+              }}
+            >
+              {"localizeId" in item
+                ? localize(item.localizeId, appState)
+                : item.text}
+            </Text>
+          )}
+        />
+      </ImageBackground>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F6EEDF',
+    backgroundColor: "#F6EEDF",
     flex: 1,
-
   },
-  list:{
-  //  backgroundColor: 'red',
-    flex:1,
-    justifyContent:'space-evenly',
-    alignContent:'center'
+  list: {
+    //  backgroundColor: 'red',
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignContent: "center",
   },
-  listItem:{
-    backgroundColor:'#e3c794',
-    color:'#F6742B',
-    textAlign:'center',
+  listItem: {
+    backgroundColor: "#e3c794",
+    color: "#F6742B",
+    textAlign: "center",
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     height: 80,
     width: 400,
-    alignSelf:'center',
-    borderRadius:20,
+    alignSelf: "center",
+    borderRadius: 20,
     shadowColor: " rgba(235,188,112,0.82)",
-  shadowOffset: {
-    width: 0,
-    height: 5,
-  },
-  shadowOpacity: 0.34,
-  shadowRadius: 6.27,
-
-  elevation: 10,
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    logoBack:{
-      flex: 1,
-      resizeMode: "cover",
-      justifyContent: "center"
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
 
-    }
+    elevation: 10,
+  },
+  logoBack: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
 });
