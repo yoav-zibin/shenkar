af86013316bf11c9444d5ccf9cf665cb7d5c7ecc
Annieli Siegel <40207237+AnnieliS@users.noreply.github.com>
2021-01-19 23:18:25 +0200

    Fixing linitng error

--- common/ChooseActivityScreen.tsx
+++ common/ChooseActivityScreen.tsx
@@ -1,4 +1,4 @@
-import {Choice, FlatListChooser} from './FlatListChooser';
+import {Choice} from './FlatListChooser';
 import React from 'react';
 import {Activity, navigateNextFrame, useStoreContext} from './store';
 import {localize, LocalizeId} from './localize';
@@ -37,70 +37,63 @@ export function ChooseActivityScreen() {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.logoBack}
-        source={require(
-          './activityBackImage.png'
-        )}
-      >
-      <TitleBar />
+      <ImageBackground style={styles.logoBack} source={require('./activityBackImage.png')}>
+        <TitleBar />
 
-  <FlatList
-      contentContainerStyle={styles.list}
-      data={choices}
-      keyExtractor={(choice) => choice.id}
-      renderItem={({item}) => (
-        <Text key={item.id} style={styles.listItem} onPress={() => {
-          dispatch({setActivity: item.data as Activity});
-          navigateNextFrame('PlayArea', navigation);
-        }}>
-          {'localizeId' in item ? localize(item.localizeId, appState) : item.text}
-        </Text>
-      )}
-    />
-</ImageBackground>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={choices}
+          keyExtractor={(choice) => choice.id}
+          renderItem={({item}) => (
+            <Text
+              key={item.id}
+              style={styles.listItem}
+              onPress={() => {
+                dispatch({setActivity: item.data as Activity});
+                navigateNextFrame('PlayArea', navigation);
+              }}>
+              {'localizeId' in item ? localize(item.localizeId, appState) : item.text}
+            </Text>
+          )}
+        />
+      </ImageBackground>
     </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F6EEDF',
     flex: 1,
-
   },
-  list:{
-  //  backgroundColor: 'red',
-    flex:1,
-    justifyContent:'space-evenly',
-    alignContent:'center'
+  list: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignContent: 'center',
   },
-  listItem:{
-    backgroundColor:'#e3c794',
-    color:'#F6742B',
-    textAlign:'center',
+  listItem: {
+    backgroundColor: '#e3c794',
+    color: '#F6742B',
+    textAlign: 'center',
     fontSize: 24,
     fontWeight: 'bold',
     height: 80,
     width: 400,
-    alignSelf:'center',
-    borderRadius:20,
-    shadowColor: " rgba(235,188,112,0.82)",
-  shadowOffset: {
-    width: 0,
-    height: 5,
-  },
-  shadowOpacity: 0.34,
-  shadowRadius: 6.27,
-
-  elevation: 10,
+    alignSelf: 'center',
+    borderRadius: 20,
+    shadowColor: ' rgba(235,188,112,0.82)',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    logoBack:{
-      flex: 1,
-      resizeMode: "cover",
-      justifyContent: "center"
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
 
-    }
+    elevation: 10,
+  },
+  logoBack: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
 });
