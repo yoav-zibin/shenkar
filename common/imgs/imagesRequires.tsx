231798d3939af7f4fa951a8c47e5f12a67ebf733
chen gutman <chengutman0@gmail.com>
2021-01-21 14:40:55 +0200

    Settings and images

---
+++ common/imgs/imagesRequires.tsx
@@ -0,0 +1,10 @@
+export const IMAGES = {
+  en: require('./en.png'),
+  he: require('./he.png'),
+  tictactoe: require('./tictactoe.png'),
+  manacal: require('./manacala.png'),
+  reversi: require('./reversi.png'),
+  go: require('./go.png'),
+  dotsandboxes: require('./dotsandboxes.png'),
+  connect4: require('./connect4.png'),
+};
