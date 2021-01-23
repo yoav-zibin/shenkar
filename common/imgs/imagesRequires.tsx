28d9843f5a3b1f557751c8b91275a9525a4b8c0d
chen gutman <chengutman0@gmail.com>
2021-01-23 20:02:02 +0200

    ChoseGameScreen
    Chen & Avital
    - adding gomoku photo
    -add Chose game screen UI

--- common/imgs/imagesRequires.tsx
+++ common/imgs/imagesRequires.tsx
@@ -2,9 +2,11 @@ export const IMAGES = {
   en: require('./en.png'),
   he: require('./he.png'),
   tictactoe: require('./tictactoe.png'),
-  manacal: require('./mancala.png'),
+  mancala: require('./mancala.png'),
   reversi: require('./reversi.png'),
   go: require('./go.png'),
   dotsandboxes: require('./dotsandboxes.png'),
   connect4: require('./connect4.png'),
+  checkers: require('./checkers.png'),
+  gomoku: require('./gomoku.png'),
 };
