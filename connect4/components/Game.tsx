d428836b880c2eadeac40c938f8f5c5db8969b36
sharon-cohen <sharon49510@gmail.com>
2021-01-23 21:25:07 +0200

    branch initilize & changes

--- connect4/components/Game.tsx
+++ connect4/components/Game.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import {StyleSheet, TouchableWithoutFeedback, View, Image, ImageBackground, ViewStyle, Switch} from 'react-native';
+import {Animated, StyleSheet, TouchableWithoutFeedback, View, Image, ImageBackground, ViewStyle} from 'react-native';
 import {GameModule, GameProps} from '../../common/common';
 
-import {createMove, IState, getInitialState, checkRiddleData} from '../gameLogic';
+import {createMove, IState, getInitialState, checkRiddleData, ROWS, Board, p, COLS} from '../gameLogic';
 import {getPossibleMoves, getStateScoreForIndex0} from '../aiService';
 import {riddleLevels} from '../riddles';
+import {BoardDelta} from '../../tictactoe/gameLogic';
 
 const hintLineColor = 'green';
 
@@ -150,7 +151,100 @@ const styles = StyleSheet.create({
     top: '5%',
   },
 });
+function getWinnerPo(board: Board) {
+  console.log('dana');
+  console.log(board);
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      if (
+        p(y, x, board) !== '0' &&
+        p(y, x, board) !== ' ' &&
+        p(y, x, board) === p(y, x + 1, board) &&
+        p(y, x, board) === p(y, x + 2, board) &&
+        p(y, x, board) === p(y, x + 3, board)
+      ) {
+        const array1: BoardDelta[] = [
+          {row: y, col: x},
+          {row: y, col: x + 1},
+          {row: y, col: x + 2},
+          {row: y, col: x + 3},
+        ];
+        return array1;
+      }
+    }
+  }
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      if (
+        p(y, x, board) !== '0' &&
+        p(y, x, board) !== ' ' &&
+        p(y, x, board) === p(y + 1, x, board) &&
+        p(y, x, board) === p(y + 2, x, board) &&
+        p(y, x, board) === p(y + 3, x, board)
+      ) {
+        const array1: BoardDelta[] = [
+          {row: y, col: x},
+          {row: y + 1, col: x},
+          {row: y + 2, col: x},
+          {row: y + 3, col: x},
+        ];
+        return array1;
+      }
+    }
+  }
 
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      for (let d = -1; d <= 1; d += 2) {
+        if (
+          p(y, x, board) !== '0' &&
+          p(y, x, board) !== ' ' &&
+          p(y, x, board) === p((y + 1) * d, x + 1, board) &&
+          p(y, x, board) === p((y + 2) * d, x + 2, board) &&
+          p(y, x, board) === p((y + 3) * d, x + 3, board)
+        ) {
+          const array1: BoardDelta[] = [
+            {row: y, col: x},
+            {row: y + 1, col: x + 1},
+            {row: y + 2, col: x + 2},
+            {row: y + 3, col: x + 3},
+          ];
+          return array1;
+        }
+      }
+    }
+  }
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      for (let d = -1; d <= 1; d += 2) {
+        if (
+          p(y, x, board) !== '0' &&
+          p(y, x, board) !== ' ' &&
+          p(y, x, board) === p((y + 1) * d, x - 1, board) &&
+          p(y, x, board) === p((y + 2) * d, x - 2, board) &&
+          p(y, x, board) === p((y + 3) * d, x - 3, board)
+        ) {
+          const array1: BoardDelta[] = [
+            {row: y, col: x},
+            {row: y + 1, col: x - 1},
+            {row: y + 2, col: x - 2},
+            {row: y + 3, col: x - 3},
+          ];
+          return array1;
+        }
+      }
+    }
+  }
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      if (p(y, x, board) === ' ') {
+        return null;
+      }
+    }
+  }
+
+  return null;
+}
 export function getConnect4Module(): GameModule<IState> {
   return {
     gameId: 'connect4',
@@ -167,21 +261,41 @@ export function getConnect4Module(): GameModule<IState> {
 const connect4Component: React.FunctionComponent<GameProps<IState>> = (props: GameProps<IState>) => {
   const {move, setMove, yourPlayerIndex, showHint} = props;
   const {turnIndex, state} = move;
-  const {riddleData} = state;
-
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const {riddleData, board, delta} = state;
+  const [boardHeight, setBoardHeight] = useState(300);
+  const animValue = new Animated.Value(0);
+  Animated.timing(animValue, {
+    toValue: 1,
+    duration: 700,
+    useNativeDriver: true,
+  }).start();
 
   function clickedOn(row: number, col: number) {
     if (turnIndex != yourPlayerIndex) {
       return;
     }
     try {
-      const move = createMove(state, row, col, turnIndex);
+      const move = createMove(state, getplace(col), col, turnIndex);
+
       setMove(move);
     } catch (e) {
       console.info('Cell is already full in position:', row, col);
     }
+  }
+
+  function getAnimationStyle(r: number) {
+    const rowHeight = boardHeight / ROWS;
+    const startYPosition = -rowHeight * (r + 1);
+    return {
+      transform: [
+        {
+          translateY: animValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [startYPosition, 0],
+          }),
+        },
+      ],
+    };
   }
 
   const rows = [0, 1, 2, 3, 4, 5];
@@ -245,58 +359,79 @@ const connect4Component: React.FunctionComponent<GameProps<IState>> = (props: Ga
     } else throw new Error('Illegal riddleData=' + riddleData);
     hintLine = <View style={style} />;
   }
+  function getplace(c: number) {
+    let i = 5;
+    while (i >= 0) {
+      if (board[i][c] == ' ') {
+        return i;
+      }
+
+      i--;
+    }
+    return -1;
+  }
+
+  function getAnimationStyleWin() {
+    return {opacity: animValue};
+  }
+  function getPiece(r: number, c: number) {
+    if (board[r][c] == ' ') return null;
+
+    const ad: BoardDelta[] | null = getWinnerPo(state.board);
+    const shouldAnimate = delta && delta.row == r && delta.col == c;
+    const animStyle = shouldAnimate && ad == null ? getAnimationStyle(r) : ad != null ? getAnimationStyleWin() : {};
+
+    if (ad != null) {
+      if (
+        (r == ad[0].row && c == ad[0].col) ||
+        (r == ad[1].row && c == ad[1].col) ||
+        (r == ad[2].row && c == ad[2].col) ||
+        (r == ad[3].row && c == ad[3].col)
+      ) {
+        return (
+          <Animated.View style={animStyle}>
+            <Image
+              style={styles.pieceImage}
+              source={state.board[r][c] == 'Y' ? require('../imgs/p1.png') : require('../imgs/p2.png')}
+            />
+          </Animated.View>
+        );
+      } else {
+        return (
+          <Image
+            style={styles.pieceImage}
+            source={state.board[r][c] == 'Y' ? require('../imgs/p1.png') : require('../imgs/p2.png')}
+          />
+        );
+      }
+    }
+    return (
+      <Animated.View style={animStyle}>
+        <Image
+          style={styles.pieceImage}
+          source={state.board[r][c] == 'Y' ? require('../imgs/p1.png') : require('../imgs/p2.png')}
+        />
+      </Animated.View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.fixedRatio}>
+      <View style={styles.fixedRatio} onLayout={(event) => setBoardHeight(event.nativeEvent.layout.height)}>
         <ImageBackground style={styles.boardImage} source={require('../imgs/connect_board.png')}>
           <View style={styles.boardRowsContainer}>
             {rows.map((r) => (
               <View key={r} style={styles.boardCellsContainer}>
-                {cols.map((c) =>
-                  r == 5 || state.board[r + 1][c] != ' ' ? (
-                    <TouchableWithoutFeedback key={c} onPress={() => clickedOn(r, c)}>
-                      <View style={styles.boardCell}>
-                        <Image
-                          style={styles.pieceImage}
-                          source={
-                            state.board[r][c] != 'Y' && state.board[r][c] != 'R' && isEnabled == true
-                              ? require('../imgs/touch.png')
-                              : null
-                          }
-                        />
-                        {state.board[r][c] != ' ' ? (
-                          <Image
-                            style={styles.pieceImage}
-                            source={state.board[r][c] == 'Y' ? require('../imgs/p1.png') : require('../imgs/p2.png')}
-                          />
-                        ) : null}
-                      </View>
-                    </TouchableWithoutFeedback>
-                  ) : (
-                    <View style={styles.boardCell} key={c}>
-                      {state.board[r][c] != ' ' ? (
-                        <Image
-                          style={styles.pieceImage}
-                          source={state.board[r][c] == 'Y' ? require('../imgs/p1.png') : require('../imgs/p2.png')}
-                        />
-                      ) : null}
-                    </View>
-                  )
-                )}
+                {cols.map((c) => (
+                  <TouchableWithoutFeedback key={c} onPress={() => clickedOn(r, c)}>
+                    <View style={styles.boardCell}>{getPiece(r, c)}</View>
+                  </TouchableWithoutFeedback>
+                ))}
               </View>
             ))}
           </View>
         </ImageBackground>
         {hintLine}
-      </View>
-      <View style={styles.bottomContainer}>
-        <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
       </View>
     </View>
   );
