2ac304d2c54a904f8305cb4eb650745dbce4a53f
chen gutman <chengutman0@gmail.com>
2021-01-30 18:44:23 +0200

    Mancala Game Changes

--- mancala/components/Game.tsx
+++ mancala/components/Game.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import {Animated, StyleSheet, TouchableWithoutFeedback, View, ImageBackground, Text, Platform} from 'react-native';
-
+import React, {useState} from 'react';
+import {localize} from '../../common/localize';
+import {useStoreContext} from '../../common/store';
+import {Animated, StyleSheet, TouchableWithoutFeedback, View, ImageBackground, Text, Dimensions} from 'react-native';
 import {GameModule, GameProps} from '../../common/common';
-
 import {
+  Board,
   createMove,
   IState,
   getInitialState,
@@ -14,122 +15,92 @@ import {
 } from '../gameLogic';
 import {getPossibleMoves, getStateScoreForIndex0} from '../aiService';
 import {riddleLevels} from '../riddles';
+const window = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  line: {
-    position: 'absolute',
-    width: 3,
-    height: 306,
-    backgroundColor: '#000',
-    transform: [{translateX: 100}],
   },
   boardImage: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'stretch',
-  },
-  GameContainer: {
-    flexDirection: 'column',
+    flex: 1,
+    resizeMode: 'cover',
     alignItems: 'center',
   },
-  BoardContainer: {
-    flexDirection: Platform.OS === 'ios' ? 'row' : 'row-reverse',
-  },
-  Player2HolesContainer: {
-    flexDirection: 'column',
-    marginRight: Platform.OS === 'ios' ? 25 : 0,
-    marginLeft: Platform.OS === 'ios' ? 0 : 25,
-    marginTop: 7,
-  },
-  Player1HolesContainer: {
-    flexDirection: 'column-reverse',
-    marginLeft: Platform.OS === 'ios' ? 25 : 0,
-    marginRight: Platform.OS === 'ios' ? 0 : 25,
-    marginTop: 7,
-  },
-  HoleContainer: {
-    flexDirection: Platform.OS === 'ios' ? 'row' : 'row-reverse',
-    alignItems: 'center',
-  },
-  HoleStyle: {
-    width: 70,
-    height: 70,
-    borderColor: '#000',
-    borderRadius: 100,
-    alignItems: 'center',
+  innerBoardContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    backgroundColor: Platform.OS === 'ios' ? 'transparent' : 'rgba(255,255,255,0.3)',
-    borderWidth: 2,
-    overflow: 'hidden',
-    shadowColor: 'black',
-    shadowRadius: 5,
-    shadowOpacity: 1,
-    marginBottom: 3,
-    marginTop: 3,
   },
-  BaseHoleContainer: {
-    width: 200,
-    height: 40,
-    backgroundColor: 'rgba(133,81,7,0.1)',
-    borderWidth: 2,
-    shadowColor: 'black',
-    shadowRadius: 3,
-    shadowOpacity: 1,
-    borderRadius: 10,
-    marginTop: Platform.OS === 'ios' ? 10 : 2,
+  BoardRow: {
+    flexDirection: 'row',
+  },
+  HolesContainer: {
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  StoneText: {
+  CellContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cellText: {
     transform: [{rotate: '270deg'}],
     fontSize: 22,
-    color: 'white',
-    fontWeight: 'bold',
-    height: 40,
+    marginLeft: 3,
+    marginRight: 3,
   },
-  Player2Title: {
-    transform: [{rotate: '90deg'}],
-    fontSize: 30,
+  StonesOuterContainerHint: {
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  StonesOuterContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.15)',
+    borderRadius: 100,
+    width: window.height / 12.8,
+    height: window.height / 12.8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '4%',
+    marginBottom: '4%',
+    marginLeft: '2%',
+    marginRight: '2%',
+  },
+  StonesInnerContainer: {
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  BaseHole: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.15)',
+    borderRadius: 100,
+    width: window.width / 2.85,
+    height: window.height / 17.92,
+  },
+  stone: {
+    height: window.height / 12.8 / 7,
+    width: window.height / 12.8 / 7,
+    borderRadius: 20,
+    margin: 1,
+    backgroundColor: '#F6742B',
+  },
+  playerText: {
+    fontSize: 28,
     color: 'white',
   },
-  Player1Title: {
-    transform: [{rotate: '270deg'}],
-    fontSize: 30,
-    color: 'white',
+  playerTextContainer: {
+    justifyContent: 'center',
   },
-  SelecterPlayerTitle: {
+  selectedPlayerText: {
     fontWeight: 'bold',
     color: '#9AE469',
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 2,
   },
-  PlayerTitleContainer: {
-    justifyContent: 'center',
-  },
-  StonesContainer: {
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-  },
-  StoneStyle: {
-    height: 10,
-    width: 10,
-    borderRadius: 20,
-    margin: 1,
-    backgroundColor: '#F6742B',
-  },
   BaseHoleText: {
-    transform: [{rotate: '270deg'}],
     fontSize: 22,
     color: 'white',
-    fontWeight: 'bold',
+    transform: [{rotate: '270deg'}],
   },
 });
 export function getMancalaModule(): GameModule<IState> {
@@ -146,105 +117,101 @@ export function getMancalaModule(): GameModule<IState> {
 }
 
 const MancalaComponent: React.FunctionComponent<GameProps<IState>> = (props: GameProps<IState>) => {
-  const {move, setMove, yourPlayerIndex} = props;
+  const {move, setMove, yourPlayerIndex, showHint} = props;
   const {turnIndex, state} = move;
   const {board} = state;
-
-  const animValue = new Animated.Value(0);
-  Animated.timing(animValue, {
+  const [prevBoard, setPrevBoard] = useState<Board>();
+  const {appState} = useStoreContext();
+  const initOpacity = new Animated.Value(0);
+  Animated.timing(initOpacity, {
     toValue: 1,
-    duration: 100,
+    duration: 1000,
     useNativeDriver: true,
   }).start();
+
   function clickedOn(playerRow: number, cell: number) {
-    console.log('BOARD', board);
-    console.info('Game , clickedOn player Row', playerRow, 'cell', cell);
+    setPrevBoard(board);
     if (turnIndex != yourPlayerIndex) {
       return;
     }
     try {
-      console.info('Game , clickedOn player Row', playerRow, 'cell', cell);
       const move = createMove(state, playerRow, cell, turnIndex);
-
       setMove(move);
     } catch (e) {
       console.info('Cell is already full in position:', playerRow, cell);
     }
   }
+
   const cols = [0, 1, 2, 3, 4, 5];
   function getStones(playerRow: number, cell: number) {
+    console.log('showHint', showHint);
+    const doShowHint = showHint && playerRow === turnIndex && board[playerRow][cell] !== 0;
     const stones = [];
+    let isDifferent = false;
+    if (prevBoard) {
+      isDifferent = prevBoard[playerRow][cell] !== board[playerRow][cell];
+    }
     for (let i = 0; i < board[playerRow][cell]; i++) {
-      stones.push(
-        <Animated.View
-          style={[
-            styles.StoneStyle,
-            {
-              opacity: animValue,
-              transform: [
-                {
-                  scale: animValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0.85, 1],
-                  }),
-                },
-              ],
-            },
-          ]}></Animated.View>
-      );
+      if (isDifferent && i === board[playerRow][cell] - 1 && !doShowHint) {
+        stones.push(<Animated.View style={[styles.stone, {opacity: initOpacity}]}></Animated.View>);
+      } else {
+        stones.push(<View style={styles.stone}></View>);
+      }
     }
     return (
-      <View style={styles.HoleContainer}>
-        <Text style={styles.StoneText}>{board[playerRow][cell]}</Text>
-        <View style={styles.HoleStyle}>
-          <View style={styles.StonesContainer}>{stones}</View>
+      <View style={styles.CellContainer}>
+        {playerRow === 1 ? <Text style={styles.cellText}>{board[playerRow][cell]}</Text> : null}
+        <View style={[styles.StonesOuterContainer, doShowHint ? styles.StonesOuterContainerHint : null]}>
+          <View style={styles.StonesInnerContainer}>{stones}</View>
         </View>
+        {playerRow === 0 ? <Text style={styles.cellText}>{board[playerRow][cell]}</Text> : null}
       </View>
     );
   }
 
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.boardImage} source={require('../imgs/wood.png')}>
-        <View>
-          <View style={styles.GameContainer}>
-            <View style={styles.BaseHoleContainer}>
-              <Text style={styles.BaseHoleText}>{board[PLAYER1_INDEX][BASE_INDEX]}</Text>
-            </View>
-            <View style={styles.BoardContainer}>
-              <View style={styles.PlayerTitleContainer}>
-                <Text style={turnIndex === 1 ? [styles.Player2Title, styles.SelecterPlayerTitle] : styles.Player2Title}>
-                  Player 2
-                </Text>
-              </View>
-              <View>
-                <View style={styles.Player2HolesContainer}>
-                  {cols.map((d) => (
-                    <TouchableWithoutFeedback key={d} onPress={() => clickedOn(1, d)}>
-                      {getStones(1, d)}
-                    </TouchableWithoutFeedback>
-                  ))}
-                </View>
-              </View>
-              <View>
-                <View style={styles.Player1HolesContainer}>
-                  {cols.map((d) => (
-                    <TouchableWithoutFeedback key={d} onPress={() => clickedOn(0, d)}>
-                      {getStones(0, d)}
-                    </TouchableWithoutFeedback>
-                  ))}
-                </View>
-              </View>
-              <View style={styles.PlayerTitleContainer}>
-                <Text style={turnIndex === 0 ? [styles.Player1Title, styles.SelecterPlayerTitle] : styles.Player1Title}>
-                  Player 1
-                </Text>
-              </View>
-            </View>
-            <View style={styles.BaseHoleContainer}>
-              <Text style={styles.BaseHoleText}>{board[PLAYER2_INDEX][BASE_INDEX]}</Text>
-            </View>
+      <ImageBackground style={styles.boardImage} source={require('../imgs/wood2.png')}>
+        <View style={styles.BaseHole}>
+          <Text style={styles.BaseHoleText}>{board[PLAYER1_INDEX][BASE_INDEX]}</Text>
+        </View>
+        <View style={styles.innerBoardContainer}>
+          <View style={styles.playerTextContainer}>
+            <Text
+              style={[
+                styles.playerText,
+                {transform: [{rotate: '90deg'}]},
+                turnIndex === 1 ? styles.selectedPlayerText : null,
+              ]}>
+              {localize('PLAYER_2_TITLE', appState)}
+            </Text>
           </View>
+          <View style={styles.HolesContainer}>
+            {cols.map((d) => (
+              <View key={d} style={styles.BoardRow}>
+                <TouchableWithoutFeedback key={d} onPress={() => clickedOn(1, d)}>
+                  {getStones(1, d)}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback key={d} onPress={() => clickedOn(0, 5 - d)}>
+                  {getStones(0, 5 - d)}
+                </TouchableWithoutFeedback>
+              </View>
+            ))}
+          </View>
+          <View style={styles.playerTextContainer}>
+            <Text
+              style={[
+                styles.playerText,
+                {transform: [{rotate: '270deg'}]},
+                turnIndex === 0 ? styles.selectedPlayerText : null,
+              ]}>
+              {localize('PLAYER_1_TITLE', appState)}
+            </Text>
+            <View></View>
+          </View>
+        </View>
+        <View style={styles.BaseHole}>
+          <Text style={styles.BaseHoleText}>{board[PLAYER2_INDEX][BASE_INDEX]}</Text>
         </View>
       </ImageBackground>
     </View>
